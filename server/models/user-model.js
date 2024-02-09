693fa99cb0de6a03baa13d1eeb2d07d6c10cd4ba
Priyanshu Bhardwaj <priyanshubhardwaj099@gmail.com>
2024-02-09 14:26:33 +0530

    secure user password bcrypt.js

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -3,6 +3,7 @@
 //Model : Acts as a higher
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -25,6 +26,25 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+//secure the password with the bcrypt
+userSchema.pre("save", async function (next) {
+  //console.log("pre method", this);
+
+  const user = this;
+
+  if (!user.isModified("password")) {
+    next();
+  }
+
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password;
+  } catch (error) {
+    next(error);
+  }
 });
 
 //define the model or the collection name
